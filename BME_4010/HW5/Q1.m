fc1b70a25d551e9c2277c88e2ee0637a6d6bfe1b
sjt85 <sjt85@cornell.edu>
2016-12-02 06:49:37 -0500

    Finished problem 1 involving nephrons

--- BME_4010/HW5/Q1.m
+++ BME_4010/HW5/Q1.m
@@ -5,25 +5,49 @@ Q1_0 = 0.125e-6;
 c0 = 120;
 
 %% Equations portion
-c1_0 = c0;
+c1_0(1,1:200) = c0;
 
 fstarL = linspace(0,1.5e-5,200);
 alpha = fstarL/(Q1_0*c0);
 
 c2_0 = c0*(1-alpha).*exp(alpha);
 
+%Total flux in and out of the loop
+flux_in = Q1_0 * c0;
+flux_out = flux_in*(1-alpha);
+flux_in(1,1:200) = flux_in;
+
 %% Plotting
 
-figure(1)
-hold on; clf; box on;
+close all
+figure
+hold on; box on;
+axis([0, 1.6e-5, 0, 150])
 
 plot(fstarL,c1_0)
 plot(fstarL,c2_0)
+%line([0,1.6e-5],[120,120],'LineStyle','--','Color','k')
+legend('c_1(0)','c_2(0)','Location','SouthWest')
 
-xlabel('Pumping rate')
-ylabel('Concentration')
+xlabel('Total pumping rate, f^*_{Na}L (mEq/min)')
+ylabel('Concentration (mEq/L)')
 
 hold off
+
+figure
+hold on; box on;
+
+plot(fstarL,flux_in)
+plot(fstarL,flux_out)
+
+legend('flux in','flux out','Location','SouthWest')
+
+xlabel('Total pumping rate, f^*_{Na}L (mEq/min)')
+ylabel('Total sodium flux (mEq/min)')
+
+hold off
+
+
 
 
 
